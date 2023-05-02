ce34bb41ff94348520785ca03186fa89bc094086
Vitalis Ogbonna <agavitalisogbonna@gmail.com>
2023-05-02 16:22:58 +0100

    updated the readme file content

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,10 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hello from mPharma!"', () => {
+      expect(appController.getHello()).toBe(
+        '<h1 style="text-align:center"> Hello, from mPharma Notification-Service! </h1>',
+      );
     });
   });
 });
